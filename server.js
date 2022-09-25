43bc8219082fd91c76cb0b38cead0023c590cacb
ozkavosh <augustoslva@hotmail.com>
2022-09-25 19:15:38 -0300

    Changed structure, socketio online users

--- server.js
+++ server.js
@@ -1,39 +1,63 @@
+//Server
 const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-//Firestore
-const admin = require("firebase-admin");
-const serviceAccount = require("./config/ecommercecoderhouse-2f872-9d1109101c0e.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const http = require("http");
+const _ = require("lodash");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-//Configs
-dotenv.config();
+//Config
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public', express.static('public'));
 
 //Routers
-const authRouter = require("./routers/authRouter");
-const dataRouter = require("./routers/dataRouter");
 app.get("/", (req, res) => {
   res.json({ status: "ok" });
 });
-app.use("/auth", authRouter);
-app.use("/data", dataRouter);
 
 //Events
 app.on("error", (err) => console.error);
 
-const server = app.listen(PORT, () => {
+const users = {};
+
+io.on("connection", (socket) => {
+  const uid = socket.handshake.query.uid;
+  
+  if(!uid) socket.disconnect();
+  if(!users[uid]) users[uid] = [];
+
+  users[uid].push(socket.id);
+
+  io.sockets.emit('connectedUsers', Object.keys(users));
+
+  console.log(users)
+
+  socket.on("disconnect", () => {
+    _.remove(users[uid], (u) => u === socket.id);
+    console.log("Se desconecto un usuario!");
+    if (users[uid].length === 0) {
+      delete users[uid];
+    }
+    console.log(users)
+    io.sockets.emit('connectedUsers', Object.keys(users));
+
+    socket.disconnect();
+  })
+});
+
+server.listen(PORT, () => {
   console.log(
     `Servidor listo y escuchando en el puerto ${server.address().port}`
   );
--- utils/generateToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = generateToken = (username) => {
-    return jwt.sign(username, process.env.JWT_SECRET, { expiresIn: '1800s' });
-}
