f75090cd2ffae74571a58606423c99efa7619961
ozkavosh <augustoslva@hotmail.com>
2022-09-25 19:35:50 -0300

    Origin list update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://proyecto-vecindario.vercel.app", /https:\/\/proyecto-vecindario-.*-ozkavosh.vercel.app/],
     methods: ["GET", "POST"],
   },
 });
